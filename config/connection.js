462dfbea8b934b6ed8a20dc5b2e6b1ab1b529b27
japankid-code <japankid.jake@gmail.com>
2021-06-20 14:43:06 -0500

    update connection for clearDB

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,29 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    pool: {
-      max: 8,
-      min: 0,
-      idle: 20000,
-      evict: 15000,
-      acquire: 30000
-    },
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+      pool: {
+        max: 8,
+        min: 0,
+        idle: 20000,
+        evict: 15000,
+        acquire: 30000,
+      },
+    }
+  );
 }
 
 module.exports = sequelize;
